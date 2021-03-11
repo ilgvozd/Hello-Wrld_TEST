4b8dc8cbda2163ba1d07f604764b43cf1641ad90
229193-6 <229193-6>
2021-03-11 12:21:34 +0300

    Отправка формы

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.name2 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(349, 39);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(404, 350);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(45, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Изменить цвет фона";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(45, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(176, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Цвет фона";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Цвет текста";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(45, 96);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(176, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.Text = "Изменить цвет текста";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(45, 211);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(176, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(227, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ширина";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(227, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Длина";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(45, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 56);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Изменить размер окна";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // name2
+            // 
+            this.name2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.name2.Location = new System.Drawing.Point(136, 320);
+            this.name2.Name = "name2";
+            this.name2.Size = new System.Drawing.Size(85, 56);
+            this.name2.TabIndex = 13;
+            this.name2.Text = "Экспорт";
+            this.name2.UseVisualStyleBackColor = false;
+            this.name2.Click += new System.EventHandler(this.name2_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button2.Location = new System.Drawing.Point(45, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 56);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.name2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button name2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(new string[] { "Красный", "Синий", "Фиолетовый",
+                "Зелёный", "Жёлтый", "Оранжевый" });
+            comboBox2.Items.AddRange(new string[] { "Красный", "Синий", "Фиолетовый",
+                "Зелёный", "Жёлтый", "Оранжевый" });
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == 0)
+                BackColor = Color.Red; 
+            if (comboBox1.SelectedIndex == 1)
+                BackColor = Color.Blue;
+            if (comboBox1.SelectedIndex == 2)
+                BackColor = Color.Purple;
+            if (comboBox1.SelectedIndex == 3)
+                BackColor = Color.Green;
+            if (comboBox1.SelectedIndex == 4)
+                BackColor = Color.Yellow;
+            if (comboBox1.SelectedIndex == 5)
+                BackColor = Color.Orange;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedIndex == 0)
+                this.textBox1.ForeColor = Color.Red;
+            if (comboBox2.SelectedIndex == 1)
+                this.textBox1.ForeColor = Color.Blue;
+            if (comboBox2.SelectedIndex == 2)
+                this.textBox1.ForeColor = Color.Purple;
+            if (comboBox2.SelectedIndex == 3)
+                this.textBox1.ForeColor = Color.Green;
+            if (comboBox2.SelectedIndex == 4)
+                this.textBox1.ForeColor = Color.Yellow;
+            if (comboBox2.SelectedIndex == 5)
+                this.textBox1.ForeColor = Color.Orange;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string W = textBox2.Text;
+            string H = textBox2.Text;
+            this.Width = Convert.ToInt32(W);
+            this.Height = Convert.ToInt32(H);
+        }
+
+        private void name2_Click(object sender, EventArgs e)
+        {
+            RegistryKey Key = Registry.CurrentUser.OpenSubKey(@"Software\NewRegKey");
+            this.Height = (int)Key.GetValue("Длина");
+            this.Width = (int)Key.GetValue("Ширина");
+            textBox1.Text = Key.GetValue("Текст").ToString();
+            int color = (int)Key.GetValue("Цвет фона");
+            int color1 = (int)Key.GetValue("Цвет текста");
+            this.BackColor = Color.FromArgb(color);
+            this.textBox1.ForeColor = Color.FromArgb(color1);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistryKey Key = Registry.CurrentUser.CreateSubKey(@"Software\NewRegKey");
+            Key.SetValue("Длина", this.Height);
+            Key.SetValue("Ширина", this.Width);
+            int color = this.BackColor.ToArgb();
+            int color1 = this.textBox1.ForeColor.ToArgb();
+            Key.SetValue("Цвет фона", color);
+            Key.SetValue("Цвет текста", color1);
+            Key.SetValue("Текст", textBox1.Text);
+        }
+    }
+}
